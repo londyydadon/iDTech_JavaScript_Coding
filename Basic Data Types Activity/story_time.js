2d111ce0688cfb4fa3ac3e48bf7c7c206f8400d4
London Nelson <typ emeial>
2022-07-06 11:56:20 -0400

    add bdt code

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,30 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greeting = "Welcome , My space subjects!";
+let name = "Queen Luna-Star";
+let title = "The Alien Queen Warrior of Space";
+let house = "Royal Castle Spaceship";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let year = 5034;
+let age = 3000028;
+let numberOfPlanets = 150;
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+ let artifacts = [ "scrolls","crystals","swords", "crowns"] ;
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let havingkidstocontinuelegacy = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log (greeting + " My name is " + name + ".");
+console.log ( "I am " + title + " and I live in my " + house);
+console.log ( " Time is different here ," +"the year is "+ year + " and I am " + age + " lightyears old.");
+console.log ( "I have been to " + numberOfPlanets + " planets" + " and have collected many things from each of them." );
+console.log ( "My favorite artifact from the planets are my " + artifacts[1]+ " but also I really like my many " + artifacts[3] + "." );
+console.log ( "Does anyone have any questions?");
+console.log ( "Random kid: You're going to have kids , to continue you legacy , right?");
+console.log ( "That is " + havingkidstocontinuelegacy + ", But that will be sometime in the future.");
+console.log ( "Now if you excuse me , I have to tend to my royal duties , Thank you for coming .")
